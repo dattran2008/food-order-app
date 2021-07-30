53b29c883b5239f8ff040abd96f194ac9ffc547e
dattran2008 <datga2006@gmail.com>
2021-07-30 16:40:55 +0700

    Update cart component

--- src/components/cart/item.jsx
+++ src/components/cart/item.jsx
@@ -1,7 +1,9 @@
-import { Button, List, Image, InputNumber } from 'antd';
+import { useState } from 'react';
+import { Button, List, Image, InputNumber, Badge } from 'antd';
 import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 
 const CartItem = (props) => {
+  const [addVal, setAddVal] = useState(props.item.amount);
   const newArr = [];
   newArr.push(props.item);
 
@@ -11,38 +13,8 @@ const CartItem = (props) => {
       size='large'
       dataSource={newArr}
       renderItem={(item) => {
-        console.log('render: ', item);
         return (
-          <List.Item
-            key={item.id}
-            actions={[
-              <>
-                <Button
-                  size='small'
-                  htmlType='button'
-                  icon={<MinusOutlined />}
-                  onClick={() => props.removeCartItem(item.id)}
-                ></Button>
-                <InputNumber
-                  size='small'
-                  min={1}
-                  max={10}
-                  defaultValue={item.amount}
-                  style={{ margin: '0 5px' }}
-                  onChange={(val) => {
-                    item = { ...item, amount: val };
-                    console.log('Change:', item);
-                  }}
-                />
-                <Button
-                  size='small'
-                  htmlType='button'
-                  icon={<PlusOutlined />}
-                  onClick={() => props.addCartItem(item)}
-                ></Button>
-              </>,
-            ]}
-          >
+          <List.Item key={item.id}>
             <List.Item.Meta
               avatar={
                 <Image
@@ -51,10 +23,56 @@ const CartItem = (props) => {
                   preview={false}
                 />
               }
-              title={<a href={item.href}>{item.name}</a>}
+              title={
+                <div>
+                  <span>{item.name}</span>
+                  <Badge
+                    count={`x${item.amount}`}
+                    offset={[10, -5]}
+                    style={{ backgroundColor: '#52c41a', fontWeight: 'bold' }}
+                  ></Badge>
+                </div>
+              }
             />
-            <div style={{ position: 'absolute', left: '88%' }}>
-              <span style={{ fontSize: '16px' }}>${item.price}</span>
+            <div style={{ display: 'flex' }}>
+              <Button
+                size='small'
+                htmlType='button'
+                icon={<MinusOutlined />}
+                onClick={() => props.removeCartItem(item.id)}
+              ></Button>
+              <InputNumber
+                size='small'
+                min={1}
+                max={99}
+                defaultValue={1}
+                style={{ margin: '0 5px' }}
+                onChange={(val) => {
+                  item = { ...item, amount: val };
+                  setAddVal(val);
+                }}
+                onStep={(val, info) => {
+                  if (info.type === 'up') {
+                    const addItem = { ...item, amount: info.offset };
+                    props.addCartItem(addItem);
+                  } else {
+                    props.removeCartItem(item.id);
+                  }
+                }}
+              />
+              <Button
+                size='small'
+                htmlType='button'
+                icon={<PlusOutlined />}
+                onClick={() => {
+                  const addItem = { ...item, amount: addVal };
+                  props.addCartItem(addItem);
+                }}
+              ></Button>
+              <div style={{ marginLeft: 'auto', fontSize: '16px', fontWeight: 600 }}>
+                <label>Price:</label>
+                <span style={{ marginLeft: '5px' }}>${item.price}</span>
+              </div>
             </div>
           </List.Item>
         );
